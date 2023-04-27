6e3b06cd0e2568c992b468d974dd1eef048afee2
Zeruel13 <63512252+Zeruel13@users.noreply.github.com>
2023-04-27 00:08:14 -0400

    Updated setup.lua
    - Checks if users are downloading for the time or updating and executes depending on that.

--- setup.lua
+++ setup.lua
@@ -10,32 +10,38 @@ shell.execute("wget -fq " .. tarMan)
 shell.setWorkingDirectory("/bin")
 shell.execute("wget -fq " .. tarBin)
 
+-- Check if updating or installing for the first time
+local isUpdating = false
+if shell.resolve("gt_MachineOS") then
+  print("Updating gt_MachineOS")
+  isUpdating = true
+else
+  print("Installing gt_MachineOS for the first time")
+  shell.execute("mkdir gt_MachineOS")
+end
+
 -- Download and extract gt_MachineOS
 local gt_MachineOS = "https://github.com/Zeruel13/gt_MachineOS/releases/latest/download/gt_MachineOS.tar"
-
 shell.setWorkingDirectory("/home")
-if not shell.resolve("gt_MachineOS") then
-    shell.execute("mkdir gt_MachineOS")
-end
 
-shell.setWorkingDirectory("/home/gt_MachineOS")
-print("\nUpdating gt_MachineOS")
 shell.execute("wget -fq " .. gt_MachineOS)
 print("...")
-if not shell.resolve("main.lua") then
-    -- Extract all files
-    shell.execute("tar -xf gt_MachineOS.tar")
+
+if isUpdating then
+  -- Extract all files except those in the addressList directory
+  shell.setWorkingDirectory("/home/gt_MachineOS")
+  shell.execute("mkdir tmp")
+  shell.execute("tar -xf gt_MachineOS.tar -C tmp")
+  shell.execute("cp -r tmp/gt_MachineOS/* .")
+  shell.execute("rm -rf tmp/gt_MachineOS/addressList")
+  shell.execute("rm -f tmp/gt_MachineOS.tar")
+  shell.execute("rmdir tmp")
 else
-    -- Extract all files except those in the addressList directory
-    shell.execute("mkdir tmp")
-    shell.execute("tar -xf gt_MachineOS.tar -C tmp")
-    shell.execute("cp -r tmp/gt_MachineOS/* .")
-    shell.execute("rm -rf tmp/gt_MachineOS")
-    shell.execute("rm -f tmp/gt_MachineOS.tar")
-    shell.execute("rmdir tmp")
+  -- Extract all files
+  shell.execute("tar -xf gt_MachineOS.tar -C gt_MachineOS")
 end
 
-shell.setWorkingDirectory("/home")
-shell.execute("rm -f setup.lua")
+-- Remove the downloaded tar file and setup script
+shell.execute("rm -f gt_MachineOS.tar setup.lua")
 
 print("Success!\n")
